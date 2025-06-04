57241fe2a572b725fc6818e9aa8da39721d42247
Minseok Song <minsk0312@naver.com>
2025-06-05 00:04:39 +0900

    GroundRobot.java 업데이트

--- GroundRobot.java
+++ GroundRobot.java
@@ -10,10 +10,10 @@ public class GroundRobot extends DeliveryRobot {
         BatteryAlarm();
 
         if (batteryLevel < 20) {
-            System.out.println(id + ": 배터리 부족으로 지상 배송 불가");
+            System.out.println(id + ": Ground delivery not possible due to low battery");
         } else {
             batteryLevel -= 30;
-            System.out.println(id + ": 지상 로봇으로 상자 배달 완료");
+            System.out.println(id + ": Box delivery completed by ground robot");
             Batterycheck();
             checkAutoRecharge();
         }
@@ -21,14 +21,14 @@ public class GroundRobot extends DeliveryRobot {
 
     public void recharge() {
         batteryLevel = 100;
-        System.out.println(id + ": 지상 로봇 배터리 충전 완료");
+        System.out.println(id + ": Ground robot battery charge complete");
     }
 
     public void BatteryAlarm() {
         if (batteryLevel < 15) {
-            System.out.println(id + ": 지상 로봇 배터리 부족 경고!");
+            System.out.println(id + ": Ground robot battery low warning!");
         } else {
-            System.out.println(id + ": 지상 로봇 배터리 상태 양호");
+            System.out.println(id + ": Ground robot battery condition is good");
         }
     }
 }
